c1b9fc0b062c62ad5efc93e7db1e1f45b368bcc2
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-23 15:06:04 +0900

    :gift: feat: 모달 열고 닫기 테스팅 추가

--- 02_basic_starting_project/cypress/e2e/basics.cy.js
+++ 02_basic_starting_project/cypress/e2e/basics.cy.js
@@ -3,5 +3,14 @@
 describe("tasks page", () => {
   it("should render the main image", () => {
     cy.visit("http://127.0.0.1:5173/");
+    cy.get(".main-header").find('img');
+    // cy.get(".main-header img");
+  });
+
+  it("should display the title", () => {
+    cy.visit("http://127.0.0.1:5173/");
+    cy.get("h1").should("have.length", 1);
+    cy.get(".main-header h1").contains("My Cypress Course Tasks");
+    // cy.contains("My Cypress Course Tasks"); // 존재하기만 한다면
   });
 });
