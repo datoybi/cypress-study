89810248765652931baa169db3ab86a9a2d87c87
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-24 12:38:42 +0900

    :gift: feat: window clipboard stub 생성 및 적용

--- 05/cypress/e2e/location.cy.js
+++ 05/cypress/e2e/location.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("share location", () => {
-  it("should fetch the user location", () => {
+  beforeEach(() => {
     cy.visit("/").then((window) => {
       cy.stub(window.navigator.geolocation, "getCurrentPosition")
         .as("getUserPosition")
@@ -15,10 +15,27 @@ describe("share location", () => {
             });
           }, 100);
         });
+      cy.stub(window.navigator.clipboard, "writeText")
+        .as("saveToClipboard")
+        .resolves();
     });
+  });
+  it("should fetch the user location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getUserPosition").should("have.been.called");
     cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
     cy.get('[data-cy="actions"]').should("contain", "Location fetched"); // contains
+  });
+
+  it("should share a location URL", () => {
+    cy.get('[data-cy="name-input"]').type("John Doe");
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+
+    cy.get("@saveToClipboard").should("have.been.called");
+    cy.get("@saveToClipboard").should(
+      "have.been.calledWithMatch",
+      new RegExp(`${37.5}.*${48.01}.*${encodeURI("John Doe")}`)
+    );
   });
 });
